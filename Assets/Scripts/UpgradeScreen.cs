fb4cc04d077ea32fdd86e0edb09572effd6cf1b7
Hugo Burd <hugo@hburd.ca>
2023-08-23 15:48:37 -0400

    upgrade buttons

--- Assets/Scripts/UpgradeScreen.cs
+++ Assets/Scripts/UpgradeScreen.cs
@@ -15,7 +15,7 @@ public class UpgradeScreen : MonoBehaviour
     void Start()
     {
         echolocation = Instantiate(upgrade_field, transform.GetChild(0)).GetComponent<Upgrade>();
-        echolocation.SetText("Echolocation");
+        echolocation.SetType(UpgradeType.Echolocation);
     }
 
     public void EnableOxygenUpgrades()
@@ -26,7 +26,7 @@ public class UpgradeScreen : MonoBehaviour
         }
 
         oxygen = Instantiate(upgrade_field, transform.GetChild(0)).GetComponent<Upgrade>();
-        oxygen.SetText("Oxygen");
+        oxygen.SetType(UpgradeType.Oxygen);
     }
 
     public void EnableSpeedUpgrades()
@@ -37,6 +37,25 @@ public class UpgradeScreen : MonoBehaviour
         }
 
         speed = Instantiate(upgrade_field, transform.GetChild(0)).GetComponent<Upgrade>();
-        speed.SetText("Speed");
+        speed.SetType(UpgradeType.Speed);
+    }
+
+    public void HandleUpgrade(UpgradeType type)
+    {
+        if (type == UpgradeType.Echolocation)
+        {
+            int cost = echolocation.GetCost();
+            echolocation.SetCost(cost + 2);
+        }
+        else if (type == UpgradeType.Oxygen)
+        {
+            int cost = oxygen.GetCost();
+            oxygen.SetCost(cost + 2);
+        }
+        else if (type == UpgradeType.Speed)
+        {
+            int cost = speed.GetCost();
+            speed.SetCost(cost + 2);
+        }
     }
 }
